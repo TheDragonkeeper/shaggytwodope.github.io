a70e750cfd788c798bd8a82a3c8afc004a878e4d
Johnathan Jenkins <twodopeshaggy@gmail.com>
2017-08-14 04:57:51 -0700

    yikes

---
+++ input/sobergrad/assets/js/custom.js
@@ -0,0 +1,81 @@
+﻿
+(function ($) {
+    "use strict";
+    var mainApp = {
+        scroll_fun: function () {
+
+            /*====================================
+             SCROLL SCRIPTS 
+            ======================================*/
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+
+            
+
+        },
+        
+        gallery_fun: function () {
+            /*====================================
+                 FOR IMAGE/GALLERY POPUP
+            ======================================*/
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+
+            /*====================================
+            FOR IMAGE/GALLERY FILTER
+            ======================================*/
+
+            // MixItUp plugin
+            // http://mixitup.io
+
+            $('#port-folio').mixitup({
+                targetSelector: '.portfolio-item',
+                filterSelector: '.filter',
+
+
+            });
+        },
+        vedio_fun:function()
+        {
+            $(function () {
+                $(".player").mb_YTPlayer();
+            });
+        },
+
+        nice_scroll_fun:function()
+        {
+            $("html").niceScroll();
+        },
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.scroll_fun();
+        mainApp.gallery_fun();
+        mainApp.vedio_fun();
+        mainApp.nice_scroll_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
+
+
